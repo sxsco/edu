62327b446828e6a543a4031adb666090a993e8ac
Shreyans Srivastava <shreyansxsrivastava@gmail.com>
2026-05-17 22:56:15 +0530

    Update index.js
    - Allow sharing between cross-origin domains...

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
-export default function handler(req, res) {
+export default function handler(req, res) {  
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    
   res.status(200).json({ message: "Hello from backend!" });
 }
