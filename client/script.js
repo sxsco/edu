534f509b5c1f6c6887a0ff997aa1da96d90e2463
shreyanssrivastava <shreyansxsrivastava@gmail.com>
2026-09-06 23:05:51 +0530

    Change in /script.js

--- client/script.js
+++ client/script.js
@@ -985,7 +985,7 @@ async function updateUserProfileWithNewImage(user, photoUrl) {
         const permanentUrl = await uploadToImgbb(photoUrl);
         if (permanentUrl) {
             await updateProfile(user, { photoURL: permanentUrl });
-       //     alert("Profile updated successfully!");
+       //    Profile updated successfully
         }
     } else {
     //    alert('photourl not found');
